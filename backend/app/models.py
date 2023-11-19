af89078b5255839dc9d28d885b53818ffc19a442
michaely8 <151398889+michaely8@users.noreply.github.com>
2023-11-19 10:01:23 -0800

    update

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=16)
-    password = models.CharField(max_length=200) # TODO: find the length of a bcrypt hash
+    password = models.CharField(max_length=200)
